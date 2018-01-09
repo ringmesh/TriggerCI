fc120b14802532cc6105f8a0d85d270fde70732e
BotellaA <arnaud.botella@total.com>
2018-01-09 11:12:14 +0100

    Create travis-trigger.py

---
+++ travis-trigger.py
@@ -0,0 +1,25 @@
+import requests, sys
+
+project=sys.argv[1]
+user_token=2h2q2pzTsgdiGoPJNqQP
+
+
+body={
+  "request": {
+    "branch":"master"
+  }
+}
+
+header={
+  "Content-Type": "application/json",
+  "Accept": "application/json",
+  "Travis-API-Version": "3",
+  "Authorization": "user_token"
+}
+
+with open(dependees/project) as file:
+    dependees = file.read().splitlines()
+
+for dependee in dependees:
+  url=https://api.travis-ci.org/repo/ringmesh%2Fdependee/requests
+  requests.post(url, data=body, headers=header)
